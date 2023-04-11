44e10f57a41b1dd748cf71d4bdd0d20f7acea564
jiangjingxue <119235879+jiangjingxue@users.noreply.github.com>
2023-04-11 03:26:04 -0400

    Delete GAHelperFunctions directory

--- GAHelperFunctions/IsTerminationSatisfied.m
+++
@@ -1,9 +0,0 @@
-function [result] = IsTerminationSatisfied(counter,limit)
-    % terminate when number of iteration exceeds the limit
-    if(counter > limit)
-        result = 1;
-    else
-        result = 0;
-    end 
-    % terminate when the fitness stop improving
-end
